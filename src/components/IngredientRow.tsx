52970acd1856393aeae0f1e23bd593c3c0ccd7d4
jssison <jssison5@up.edu.ph>
2026-05-06 13:22:47 +0800

    style: better responsive layout for nutrient details

--- src/components/IngredientRow.tsx
+++ src/components/IngredientRow.tsx
@@ -128,11 +128,11 @@ export default function IngredientRow({ row, speciesGroup, usedKeys, onChange, o
           </div>
         </Field>
       </div>
-      <div className="flex gap-4 text- text-slate-500 pt-1">
+      <div className="flex flex-col lg:flex-row gap-1 lg:gap-4 text- text-slate-500 pt-1">
         <span className="text-xs font-semibold uppercase tracking-widest text-slate-400">
           Protein: {" "}
           <span className="font-medium text-slate-800">{INGREDIENTS[row.ingredientKey].nutrients.protein}</span>
-          <span className="font-medium text-slate-800">{" "}%</span>
+          <span className="font-medium text-slate-800">%</span>
         </span>
         <span className="text-xs font-semibold uppercase tracking-widest text-slate-400">
           Metabolizable Energy: {" "}
